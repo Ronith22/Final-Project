cf5341de0c726495eaade1ceeb0dfeb05e04794e
ronithanchan <ronith.anchan@gmail.com>
2023-11-10 23:50:53 -0500

    Updated README File with specific tasks to be completed

--- Project-Code.py
+++ Project-Code.py
@@ -1,5 +1,6 @@
 from designer import *
 from dataclasses import dataclass
+KNIGHT_SPEED = 5
 @dataclass
 class Game_World:
     knight: image
@@ -7,15 +8,36 @@ class Game_World:
     score: int
 
 
-
-knight = image("https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/840386/knight-clipart-md.png")
-grow(knight, .1)
-draw(knight)
+#Knight Object, supposed to be the playable character
+Knight = image("https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/840386/knight-clipart-md.png")
+grow(Knight, .1)
+draw(Knight)
 
 def create_game_world()-> Game_World:
     """ Create the world """
-    return Game_World(knight, 0, 0)
+    return Game_World(Knight, 0, 0)
 
-def move_knight()
+def move_knight(game_world: Game_World):
+    pass
+def create_knight():
+    knight = Knight
+    return knight
 
-#when("updating", create_knight)
+def head_left(game_world: Game_World):
+    """ Make the knight start moving left """
+    game_world.knight_speed = -(KNIGHT_SPEED)
+
+def head_right(game_world: Game_World):
+    """ Make the knight start moving left """
+    game_world.knight_speed = KNIGHT_SPEED
+def flip_knight(game_world: Game_World, key: str):
+    """ Change the direction that the knight is moving """
+    if key == "left":
+        head_left(world)
+    elif key == "right":
+        head_right(world)
+
+when("updating", create_knight)
+when('starting', create_game_world)
+
+start()
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Final-Project
 Game: Scale Search
 You, the knight, will go around and defeat various enemies in pursuit of recollecting various lost
-"scales" of increasing tiers. 
+"scales" of increasing tiers. Intended to be a quiz game. 
+
 Tasks underway:
 -Get Knight to move
 -'Animate' Knight performing tasks
